3b0b81456e9ff1efff949a0877c6d91d0b0afbbc
Paolo Rampino <info@tuttoainternet.it>
2014-06-22 19:57:32 +0200

    Fix on theme-helper scripts
    Now themes will be add on a build directory, in order to be checking.

--- customize/theme-helper/backup/redo.sh
+++
@@ -1,2 +0,0 @@
-!#/bin/bash
-cp -a /home/user/install/cinelerra-2.1.5/cinelerra-theme-helper/backup/cinelerra /home/user/install/cinelerra-2.1.5/cinelerra-theme-helper/backup/plugins /home/user/install/cinelerra-2.1.5/cinelerra-theme-helper/backup/redo.sh /home/user/install/cinelerra-2.1.5/cinelerra-theme-helper/../../
